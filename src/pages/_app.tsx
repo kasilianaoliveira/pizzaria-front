2eca30ec3edde84ae96b753a74480f61ec350ed8
kasilianaoliveira <kasilianaoliveira@gmail.com>
2023-06-15 16:20:24 -0300

    Initial config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import '@/styles/globals.scss';
+import type { AppProps } from 'next/app';
+import { Poppins } from 'next/font/google';
+ 
+const poppins = Poppins({
+  variable: '--font-poppins',
+  subsets:['latin'],
+  weight:['700','600', '500','400']
+})
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
